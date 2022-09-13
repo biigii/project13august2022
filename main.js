a63c2553634169d63c23ff1a07bce42b19b27809
behshad <82580731+biigii@users.noreply.github.com>
2022-09-13 20:26:20 +1200

    js 1

---
+++ main.js
@@ -0,0 +1,29 @@
+
+// document.getElementById("heading").style.color = 'red';
+// document.getElementById('heading').style.backgroundColor = 'yellow';
+// document.getElementById('heading').style.fontSize = '60px';
+
+// document.querySelector('#heading').style.color = '#f33';
+// document.querySelector(".title").style.backgroundColor = '#ff0';
+// document.querySelector('h1').style.fontSize = '5em';
+// document.querySelector("[data-something]").style.textAlign = 'center'
+
+// document.querySelector('[src="https://picsum.photos/id/1031/100"]').style.border = '10px solid #000'
+// document.querySelector('[data-photo]').style.border = '10px solid #000'
+
+// const photo = document.querySelector('[data-photo]');
+// photo.style.border = '3px dashed #f33';
+// photo.style.width = '200px';
+
+// const h1 =  document.querySelector('h1');
+// h1.style.fontSize = '100px';
+// h1.style.textAlign = 'center';
+
+
+// document.querySelector('[data-photo]').setAttribute('class', 'photo');
+// document.querySelector('[data-photo]').setAttribute('alt','some alt value');
+
+// document.querySelector('#text').classList.add('text');
+// document.querySelector('#heading').classList.remove('title');
+
+// document.querySelector('#heading').classList.toggle('title');
--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ https://codepen.io/behshad/pen/abdXxNg
 
 https://codepen.io/behshad/pen/OQZKLw
 https://flexboxfroggy.com/
+
+https://codepen.io/behshad/pen/wmdeqO?editors=1010
